aa1b796de160b5b63effb5e323becc0fa4d9eea0
ViniMacacari <macacarivinicius@gmail.com.br>
2025-04-04 16:31:12 -0300

    feat: trativa de erro na stream

--- src/controllers/users-rooms.ts
+++ src/controllers/users-rooms.ts
@@ -33,20 +33,32 @@ export class UsersRoomsController {
 
         const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
         let lastCount = 0
+        let tentativas = 0
 
         const sendUpdates = async () => {
             while (true) {
                 const data = await this.service.get(Number(roomCode))
 
-                if (data.length !== lastCount) {
-                    lastCount = data.length
-                    res.write(`data: ${JSON.stringify(data)}\n\n`)
-                }
+                if (!data || data.length === 0) {
+                    tentativas++
 
-                if (data.length >= 4) {
-                    res.write('event: close\ndata: done\n\n')
-                    res.end()
-                    break
+                    if (tentativas >= 3) {
+                        res.write('event: error\ndata: Sala não encontrada ou esvaziada\n\n')
+                        res.end()
+                        break
+                    }
+                } else {
+                    tentativas = 0
+                    if (data.length !== lastCount) {
+                        lastCount = data.length
+                        res.write(`data: ${JSON.stringify(data)}\n\n`)
+                    }
+
+                    if (data.length >= 4) {
+                        res.write('event: close\ndata: done\n\n')
+                        res.end()
+                        break
+                    }
                 }
 
                 await delay(1000)
