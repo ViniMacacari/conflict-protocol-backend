4b542e1adddea90a59e275c8a7dc1ad6c3d17c85
ViniMacacari <macacarivinicius@gmail.com.br>
2025-04-07 14:42:25 -0300

    feat: mínimo de tempo por jogada é de 60s

--- src/controllers/new-room.ts
+++ src/controllers/new-room.ts
@@ -8,6 +8,10 @@ export class NewRoomController {
         try {
             const { name, user, time } = req.body
 
+            if (time < 60) {
+                return res.status(400).json({ error: "O tempo mínimo é de 60 segundos." })
+            }
+
             const result = await this.service.newRoom({ name, user, time })
 
             return res.status(201).json({ roomCode: result?.[0]?.p_codigo })
